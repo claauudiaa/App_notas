fd9efe0ee92fbadf33d14f4b13a492bf357a8c49
CLAUDIA CANDELAS OVIEDO <claudiacandelas2005@gmail.com>
2025-01-23 19:34:54 +0100

    Ya está la parte de ver nota, falta la de agregar

--- app/src/main/java/com/example/app_notas/MainActivity.java
+++ app/src/main/java/com/example/app_notas/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.app_notas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,16 +19,16 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    private RecyclerView recyclerView;
+    private ListView listView;
     private FloatingActionButton botonAgregar;
-    private ArrayList<Nota> notas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recyclerView);
+
+        listView = findViewById(R.id.listView);
         botonAgregar = findViewById(R.id.botonFlotante);
 
         ArrayList<Nota> notas = new ArrayList<Nota>();
@@ -32,12 +36,33 @@ public class MainActivity extends AppCompatActivity {
         notas.add(new Nota("COMPRA", "Huevos, tomate, chorizo"));
         notas.add(new Nota("Contraseñas", "GitHub: 1234"));
 
-        recyclerView.setAdapter(new Adaptador(this, R.layout.entrada, notas) {
+        listView.setAdapter(new Adaptador(this, R.layout.entrada, notas) {
             @Override
             public void onEntrada(Object entrada, View view) {
+                if (entrada != null) {
+                    TextView texto_titulo = (TextView) view.findViewById(R.id.texto_titulo);
+                    TextView texto_contenido = (TextView) view.findViewById(R.id.texto_contenido);
 
+                    texto_titulo.setText(((Nota) entrada).getTitulo());
+                    texto_contenido.setText(((Nota) entrada).getContenido());
+
+                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            Nota selectedNota = notas.get(position);
+
+                            Intent intent = new Intent(MainActivity.this, ViewNoteActivity.class);
+
+                            intent.putExtra("titulo", selectedNota.getTitulo());
+                            intent.putExtra("contenido", selectedNota.getContenido());
+
+                            startActivity(intent);
+                        }
+                    });
+
+                }
             }
-        }
+        });
     }
 
     public class Nota {
--- app/src/main/java/com/example/app_notas/ViewNoteActivity.java
+++ app/src/main/java/com/example/app_notas/ViewNoteActivity.java
@@ -1,6 +1,9 @@
 package com.example.app_notas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +11,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class ViewNoteActivity extends AppCompatActivity {
+
+    FloatingActionButton btnAtras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_note);
+
+        btnAtras = findViewById(R.id.botonFlotanteAtras);
+
+        Intent intent = getIntent();
+        String titulo_detalle = intent.getStringExtra("titulo");
+        String contenido_detalle = intent.getStringExtra("contenido");
+
+        TextView textoTitulo = findViewById(R.id.texto_titulo_detalle);
+        TextView textoContenido = findViewById(R.id.texto_contenido_detalle);
+
+        textoTitulo.setText(titulo_detalle);
+        textoContenido.setText(contenido_detalle);
+
+
+        btnAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intento = new Intent(ViewNoteActivity.this, MainActivity.class);
+                startActivity(intento);
+            }
+        });
+
+
     }
 }
